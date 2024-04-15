4bf76157d956505c9110d49a5f82d39f27cd1858
RaphaelsHub <spekel2003@gmail.com>
2024-04-15 17:40:20 +0300

    Selection, Bubble, Insertion sort

--- CPP/SortAlg/SortAlg.cpp
+++ CPP/SortAlg/SortAlg.cpp
@@ -1,57 +1,9 @@
+#include "SortingAlgorithms/3.BoobleSort.h"
+#include "SortingAlgorithms/4.InsertionSort.h"
+#include "SortingAlgorithms/5.SelectionSort.h"
 
-void buble_sort(int* arr)
-{
-    for (int i = 0; i < 10; i++)
-    {
-        for (int x = i+1; x < 10; x++)
-        {
-            if(arr[i]>arr[x])
-            {
-                const int tmp = arr[x];
-                arr[x]=arr[i];
-                arr[i]=tmp;
-            }
-        }
-    }
-}
 
-void InsertionSort(int* arr)
-{
-    for (int i=1;i<10;i++)
-    {
-        const int key = arr[i];
-        int ind=0;
-        for (int x=i-1;i>=0 && arr[x]>key; i--)
-        {
-            if(arr[x]>arr[i])
-            {
-                arr[i]=arr[x];
-            }
-            ind=x;
-        }
-        arr[ind]=key;
-    }
-}
 
-void MinSort(int* arr)
-{
-    for (int i = 0; i < 10-1; i++)
-    {
-        int min_index=i;
-        for (int x = min_index+1; x < 10; x++)
-        {
-            if(arr[x]<arr[min_index])
-            {
-                min_index = x;
-            }
-        }
-
-        const int tmp = arr[i];
-        arr[i]=arr[min_index];
-        arr[min_index]=tmp;
-
-    }
-}
 
 void BinarySearch(const int* arr)
 {
@@ -67,11 +19,13 @@ void BinarySearch(const int* arr)
     }
 }
 
+
 int main()
 {
     int nums[10] = {2,6,8,2,3,5,7,8,5,2};
-    buble_sort(nums);
-    InsertionSort(nums);
-    MinSort(nums);
-    BinarySearch(nums);
+    BubleSort(nums,10);
+    int nums1[10] = {2,6,8,2,3,5,7,8,5,2};
+    InsertionSort(nums1, 10);
+    int nums2[10] = {2,6,8,2,3,5,7,8,5,2};
+    MinSort(nums2);
 }
